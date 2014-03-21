2b500ab73ce46157461751fecac11eb52b10165d
Matt Durgavich <mattdurgavich@Matts-MacBook-Air.local>
2014-03-20 23:56:29 -0400

    Remove

---
+++ Quiver/Quiver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quiver
+//
+//  Created by Matt Durgavich on 3/20/14.
+//  Copyright (c) 2014 WymzeeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiver/Quiver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiver
+//
+//  Created by Matt Durgavich on 3/20/14.
+//  Copyright (c) 2014 WymzeeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
